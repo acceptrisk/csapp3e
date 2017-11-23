e6c2286c58fb03b35e012892e08255fe89c56592
acceptrisk <foxmike@sina.com>
2017-11-23 22:34:43 +0800

    Update arraysize.cc

--- arraysize.cc
+++ arraysize.cc
@@ -1,6 +1,6 @@
 // Example program
 #include <iostream>
-#include <string>
+#include <array>
 
 template<typename T, std::size_t N>
 constexpr std::size_t ArraySize(T (&)[N])
@@ -12,5 +12,8 @@ int main()
 {
     int aKeys[] = {1, 2, 3, 4};
     int aKeysOther[ArraySize(aKeys)];
-    std::cout << sizeof(aKeysOther) << std::endl;
+    std::cout << "sizeof(aKeysOther) "  << sizeof(aKeysOther) << std::endl;
+    
+    std::array<int, ArraySize(aKeys)> aKeysAnother;
+    std::cout << "sizeof(aKeysAnother) " << sizeof(aKeysAnother) << std::endl;
 }
