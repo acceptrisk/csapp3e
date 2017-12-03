26048cb8c84caee2c853f6b28afff48743eb8223
acceptrisk <foxmike@sina.com>
2017-12-03 16:33:16 +0800

    Update RefObject.hh

--- RefObject.hh
+++ RefObject.hh
@@ -1,11 +1,12 @@
 #pragma once
 #include <atomic>
 
+/// @class RefObject - template class to support reference count
 template <typename T>
 class RefObject : public T
 {
 public:
-    RefObject():refCount_(1)
+    RefObject() : refCount_(1)
     {}
 
     virtual ~RefObject()
@@ -15,21 +16,21 @@ public:
     RefObject(T1&& t1, Tn&&... tn) : T(t1, tn...), refCount_(1)
     {}
 
-    RefObject(const RefObject& rhs):refCount_(1)
+    RefObject(const RefObject& rhs) : refCount_(1)
     {}
 
     RefObject& operator=(const RefObject& rhs)
     {
         static_cast<T&>(*this) = rhs;
         return (*this);
-    }
+    } 
 
     virtual void Destroy()
     {
         delete this;
     }
 
-    void Ref()
+    void Ref() noexcept
     {
         ++refCount_;
     }
@@ -39,14 +40,13 @@ public:
         if (--refCount_ == 0)
         {
             Destroy();
-        }        
+        }
     }
-    int Use_count() const
+    int Use_count() const noexcept
     {
         return (int)refCount_;
     }
 
-private:        
+private:
     std::atomic<int> refCount_;
 };
-
